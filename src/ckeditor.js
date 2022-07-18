6b89ca7898ec04fc96636416a050c995ab207934
Ezequiel Calcanat <79470817+EzequielCalcanat@users.noreply.github.com>
2022-07-18 12:05:45 -0500

    Add files via upload

---
+++ src/ckeditor.js
@@ -0,0 +1,54 @@
+/**
+ * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Alignment,
+	Bold,
+	Essentials,
+	FontColor,
+	FontFamily,
+	FontSize,
+	Heading,
+	HorizontalLine,
+	Italic,
+	Link,
+	Paragraph
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'bold',
+			'fontSize',
+			'fontColor',
+			'italic',
+			'|',
+			'undo',
+			'redo',
+			'alignment'
+		]
+	},
+	language: 'en'
+};
+
+export default Editor;
